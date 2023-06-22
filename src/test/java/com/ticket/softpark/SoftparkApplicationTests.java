e9b3758fcdbb48b0d588aedc8b691f45cb97715f
jacksuh <jacksonsdss@gmail.com>
2023-06-21 23:31:39 -0300

    Commit first project

---
+++ src/test/java/com/ticket/softpark/SoftparkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticket.softpark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftparkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
