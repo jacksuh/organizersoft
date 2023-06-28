9426ee1f2e75907654f4f5b243191ec7abed8af7
Jackson <jacksonsdss@gmail.com>
2023-06-28 10:15:48 -0300

    Commit: private, status

--- src/main/java/com/ticket/softpark/dto/TicketDto.java
+++ src/main/java/com/ticket/softpark/dto/TicketDto.java
@@ -1,11 +1,14 @@
 package com.ticket.softpark.dto;
+
+import com.ticket.softpark.model.Status;
+
 public record TicketDto(
         String ticket,
         String prioridade,
         String descricao,
         String desenvolvedor,
         String autor,
-        String status,
+        Status status,
         String prazo
 ) {
 }
