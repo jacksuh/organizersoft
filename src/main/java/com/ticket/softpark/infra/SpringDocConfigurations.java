ee0297dcf4caf5dad20041720699868b9e517a92
jacksuh <jacksonsdss@gmail.com>
2023-07-11 21:00:07 -0300

    DOC Swagger

---
+++ src/main/java/com/ticket/softpark/infra/SpringDocConfigurations.java
@@ -0,0 +1,16 @@
+package com.ticket.softpark.infra;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringDocConfigurations {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .components(new Components()
+                        .addSecuritySchemes("bearer-key",
+                                new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")))
+    }
+}
