888cde1410399a4eb51d6a45d86d2bdae30f0939
jacksuh <jacksonsdss@gmail.com>
2023-07-13 21:02:56 -0300

    DOC Swagger

--- src/main/java/com/ticket/softpark/infra/SpringDocConfigurations.java
+++ src/main/java/com/ticket/softpark/infra/SpringDocConfigurations.java
@@ -2,6 +2,9 @@ package com.ticket.softpark.infra;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,6 +17,15 @@ public class SpringDocConfigurations {
         return new OpenAPI()
                 .components(new Components()
                         .addSecuritySchemes("bearer-key",
-                                new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")));
+                                new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")))
+                .info(new Info()
+                        .title("Ticket")
+                        .description("API Rest Ticket")
+                        .contact(new Contact()
+                                .name("Jackson Santos")
+                                .email("jacksonsdss@gmail.com"))
+                        .license(new License()
+                                .name("Jackson")
+                                .url("https://jacksuh.github.io/portifolio-page/")));
     }
 }
